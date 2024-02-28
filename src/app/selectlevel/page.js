b46b7d284c941372efb4b4e1040ee70703d10c28
Rossiny O. St Cyr <rossinyostcyr@gmail.com>
2024-02-28 18:06:45 -0500

    slct-lvl page - part 2

--- src/app/selectlevel/page.js
+++ src/app/selectlevel/page.js
@@ -1,16 +1,30 @@
 import Link from "next/link";
-import "../global.css"
+import "../global.css";
+import {data} from "./SL-data.js";
 
 export default function selectlevel() {
+
+  const { level} = data;
+
   return (
+
+   
     <div className="container mx-auto p-8" >
       <h2>Select level</h2>
 
-        <Link href="l1">
-      <div className="bg-green-500 m-8 p-8 flex flex-col items-center" >
-          <h3 className="">Level 1</h3>
-      </div>
-        </Link>
+          <div className="columns-2 m-3 p-3">
+      {level.map((lvl) => {
+        return (
+            
+          <div className="w-24 border border-black m-2 p-2" >
+          <Link href={lvl.href}>
+              <h3 className="">{lvl.title}</h3>
+            </Link>
+          </div>
+        );
+      })
+    };
+    </div>
     </div>
   );
 }
