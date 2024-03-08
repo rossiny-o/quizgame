a0a01683910a52fa99e24af6b94b7b883330e915
Rossiny O. St Cyr <rossinyostcyr@gmail.com>
2024-03-07 19:43:30 -0500

    questions page - pt2

--- src/app/questions/page.js
+++ src/app/questions/page.js
@@ -5,8 +5,9 @@ export default function questions(){
     return (
         <div className="container mx-auto py-8">
             {/* first row  */}
-            <div className="flex items-center space-x-10 m-3 p-3">
-                <h5>Question 1/10</h5>
+            <div className="flex justify-items-center space-x-10 m-3 p-3">
+                <h5>Question: 1/10</h5>
+                <h4>Level: 1</h4>
                 <h4>Round: 1</h4>
                 <h5>Time: 45s</h5>
                 <h5>Score: 0</h5>
@@ -15,7 +16,7 @@ export default function questions(){
             {/* question & answer */}
             <div className="m-3 p-3">
                 <h3 className="p-3 font-bold">Select the correct answer: </h3>
-                <div className="flex m-2 border-2 border-black">
+                <div className="flex m-2 border-2 border-black rounded-r">
                     <p className="p-2 px-4 mr-3 border-r-2 border-black">A</p>
                     <p className="p-1">Choice 1</p>
                 </div>
@@ -33,8 +34,10 @@ export default function questions(){
                 </div>
             </div>
             <div className="m-3 p-3">
-                <button className="px-3 py-1 bg-blue-500 text-white rounded-full">Previous</button>
-                <button className="px-3  py-1 bg-blue-500 text-white float-end rounded-full">Submit</button>
+                <button className="font-semibold rounded-lg border border-gray-700 px-3 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">Previous</button>
+
+                
+                <button className="float-end font-semibold rounded-lg border border-gray-700 px-3 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">Submit</button>
             </div>
         </div>
     )
--- src/app/selectlevel/page.js
+++ src/app/selectlevel/page.js
@@ -8,12 +8,12 @@ export default function selectlevel() {
   return (
     <div className="container mx-auto py-10">
       <h2>Select level</h2>
-
+      
       <div className="mx-auto flex flex-wrap gap-x-1">
         {level.map((lvl) => {
           return (
             <div className="box-border w-32 h-32 border-2 border-black m-2 p-2">
-              <Link href={lvl.href}>
+              <Link href="/questions">
                 <h3 className="">{lvl.title}</h3>
               </Link>
             </div>
